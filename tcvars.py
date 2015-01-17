526b80461ced6ca3faca57cba86d3e44eb05f5f6
pytcrrd <robert.lacina@gmail.com>
2015-01-17 20:55:46 +0100

    base functions

--- tcvars.py
+++ tcvars.py
@@ -1,9 +1,9 @@
-#lan = 'eth1'
-lan = 'wlan0'
+lan = 'eth1'
+#lan = 'wlan0'
 wan = 'eth0'
 trafic = 'True'
 wwwpath = '/var/www/html/pyiptabrrd/'
-wwwhead = '<html><head><title>PyTcRRD</title></head><body><table><tr><td>LAN - download</td><td>WAN - upload</td></tr>'
+wwwhead = '<html><head><title>PyIptabRRD</title></head><body><table><tr><td>LAN - download</td><td>WAN - upload</td></tr>'
 wwwfooter = '</table></body></html>'
 iptrafgrafs = [
     '192.168.222.2',
