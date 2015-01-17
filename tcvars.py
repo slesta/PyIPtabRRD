501d3d6612a0a1606a128c41c9a707a93a14a6f2
pytcrrd <robert.lacina@gmail.com>
2015-01-17 19:10:04 +0100

    setup vars

--- tcvars.py
+++ tcvars.py
@@ -2,6 +2,6 @@
 lan = 'wlan0'
 wan = 'eth0'
 trafic = 'True'
-wwwpath = '/var/www/html/pytcrrd/'
+wwwpath = '/var/www/html/pyiptabrrd/'
 wwwhead = '<html><head><title>PyTcRRD</title></head><body><table><tr><td>LAN - download</td><td>WAN - upload</td></tr>'
 wwwfooter = '</table></body></html>'
